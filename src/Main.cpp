f7212d05ab9fff32c28cbd2551c85bcd04651c53
Blair Burton <blair.burton@outlook.com>
2021-08-30 13:43:33 +1200

    Add default files

---
+++ src/Main.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file Main.cpp
+ * @author
+ * @brief 
+ * @version 0.1
+ * @date
+ * 
+ * @copyright Copyright (c) 20xx
+ * 
+ */
+#include <iostream>
+
+int main(void)
+{
+	std::cout << "Hello World!" << std::endl;
+
+	std::cin.get(); // Pause program at end
+}
